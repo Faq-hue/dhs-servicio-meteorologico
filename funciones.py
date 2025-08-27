75b1901cc6befcd578894308ee77040ca37e8c08
Faqhue <fzamora994@alumnos.iua.edu.ar>
2025-08-26 21:57:44 -0300

    fucniones de amplitudes de estaciones completadas

--- funciones.py
+++ funciones.py
@@ -1,11 +1,11 @@
 
 #Un reporte por estación meteorológica de la temperatura máxima y mínima registradas en el período del último año.
-def maxminPorAño(datos):
+def maxminPorAño(datos, anio):
     
     for estacion in datos :
         max = -1000
         min = 1000
-        
+                
         for temperaturaMaxima in datos[estacion]["tmax"]:
             
             if temperaturaMaxima != None:
@@ -17,22 +17,56 @@ def maxminPorAño(datos):
             if temperaturaMinima != None:
                 if min > temperaturaMinima:
                     min = temperaturaMinima
-            
-                
+                        
         print(str(estacion)+" temperatura maxima: " + str(max))
         print(str(estacion)+" temperatura minima: " + str(min))
-        
-        
-        
-    
+
     return 1
 
 #La estación meteorológica que registre la mayor amplitud térmica en el mismo día, indicando el día del año que ocurrió
-def estacionMayorAmplitud():
+def estacionMayorAmplitud(datos):  
+    amplitudVieja = -1000.0
+    estacionMayor, fechaMayor = None, None
+    
+    for estacion, valores in datos.items():
+        fechas = valores["fecha"]
+        tmaxs = valores["tmax"]
+        tmins = valores["tmin"]
+    
+        for fecha, tmax, tmin in zip(fechas, tmaxs, tmins):
+            if tmax is not None and tmin is not None:
+                amplitud = abs(tmax - tmin)
+
+                if amplitudVieja < amplitud:
+                    amplitudVieja = amplitud
+                    estacionMayor = estacion
+                    fechaMayor = fecha
+    
+    print("Estacion con mayor amplitud: " + str(estacionMayor) + "\nen la fecha de:" + str(fechaMayor))
+    
     return 1
 
 #La estación meteorológica que registre la menor amplitud térmica en el mismo día, indicando el día del año que ocurrió
-def estacionMenorAmplitud():
+def estacionMenorAmplitud(datos):
+    amplitudVieja = float("inf")
+    estacionMenor, fechaMenor = None, None
+    
+    for estacion, valores in datos.items():
+        fechas = valores["fecha"]
+        tmaxs = valores["tmax"]
+        tmins = valores["tmin"]
+    
+        for fecha, tmax, tmin in zip(fechas, tmaxs, tmins):
+            if tmax is not None and tmin is not None:
+                amplitud = abs(tmax - tmin)
+
+                if amplitudVieja > amplitud:
+                    amplitudVieja = amplitud
+                    estacionMenor = estacion
+                    fechaMenor = fecha
+    
+    print("Estacion con menor amplitud: " + str(estacionMenor) + "\nen la fecha de:" + str(fechaMenor))
+    
     return 1
 
 #La máxima diferencia de temperatura entre minima y máxima temperatura entre dos estaciones meteorológicas en un mismo día, indicando las temperaturas y las estaciones que las registraron.
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import administradorArchivo, funciones
 
 datos = administradorArchivo.leerArchivo("registro_temperatura365d_smn.txt")
-#print(datos["AZUL AERO"]["tmin"])
+#print(datos["AZUL AERO"]["fecha"])
 
-funciones.maxminPorAño(datos)
+#funciones.maxminPorAño(datos)
+funciones.estacionMayorAmplitud(datos)
+funciones.estacionMenorAmplitud(datos)
+
